10686ae94a0e8791594f89f8024b797deabacd69
Alexey <mister.skorpion4ik@mail.ru>
2026-01-19 19:37:30 +0500

    basic settings project

---
+++ wb_parser/parser.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests
+
+
+search = "пальто из натуральной шерсти"
+base_url = f"https://www.wildberries.ru/catalog/0/search.aspx?search={search}"
+
+
+def get_item_info():
+    pass
